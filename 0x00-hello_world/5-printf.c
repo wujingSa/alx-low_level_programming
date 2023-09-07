161648cc15f6e51c8286121d6316e5d4ff583bda
wujingSa <samojuade1@gmail.com>
2023-09-07 15:03:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always (0)
- *
+ * Return: 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
